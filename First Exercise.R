b9426d6252fb253875b234f482caa57233db41c0
atarporat <atarkotu@gmail.com>
2023-04-08 22:11:37 +0300

    Added First Exercise code
    😊

---
+++ First Exercise.R
@@ -0,0 +1,30 @@
+ install.packages(c("tidyverse", "kableExtra"))
+ library(dplyr)
+ library(knitr)
+ library(kableExtra)
+ library(magrittr)
+ data(iris)
+ iris %>%
+   select(starts_with("Sepal"), Species) %>%
+   group_by(Species) %>%
+   summarise(Average_Sepal_Length = mean(Sepal.Length)) %>%
+   kable() %>%
+   kable_styling()
+ library(ggplot2)
+ 
+ data("mtcars")
+ mtcars %>%
+   mutate(cyl = as.factor(cyl)) %>%
+   ggplot(aes(x = hp, y = mpg, color = cyl)) +
+   geom_point() +
+   geom_smooth(method = "lm", size = 1)
+ install.packages("usethis")
+ 
+ library(usethis)
+ 
+ use_git_config(
+   scope = "project",
+   user.name = "atarporat",
+   user.email = "atarkotu@gmail.com"
+ )
+ 
